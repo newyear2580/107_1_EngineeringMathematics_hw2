248e74afaf777a37504cb14548a7570e9d796ea1
newyear2580 <51331397+newyear2580@users.noreply.github.com>
2022-08-08 13:23:00 +0800

    Create 1-c.py

---
+++ 1-c.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 00:47:39 2018
+
+@author: abc
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import *              # 導入sympy
+
+def f(x) :
+    return x*np.sin(x)
+
+def g(x) :
+    return x**2 - x**4/(3*2) + x**6/(5*4*3*2)
+
+x = np.linspace(-np.pi, np.pi, 100)
+y1 = f(x)
+y2 = g(x)
+plt.plot(x, y1, '-', x, y2, '--')
+
+plt.xlabel('x')
+plt.ylabel('f(x) & g(x)')
+plt.title('Plot the functions x*sinx and x^2 - x^4/(3!) + x^6/(5!)')
+plt.text(1, 0, 'Copyright@Chen Li Wei')
+
+plt.show()
