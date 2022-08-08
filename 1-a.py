d03026ebbe7adbf78fb6c4de4c83276b0f0d6198
newyear2580 <51331397+newyear2580@users.noreply.github.com>
2022-08-08 13:22:26 +0800

    Create 1-a.py

---
+++ 1-a.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 00:34:59 2018
+
+@author: abc
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import *              # 導入sympy
+
+def f(x) :
+    return x*np.sin(x)
+
+def g(x) :
+    return x**2
+
+x = np.linspace(-np.pi, np.pi, 100)
+y1 = f(x)
+y2 = g(x)
+plt.plot(x, y1, '-', x, y2, '--')
+
+plt.xlabel('x')
+plt.ylabel('f(x) & g(x)')
+plt.title('Plot the functions x*sinx and x^2')
+plt.text(1, 0.2, 'Copyright@Chen Li Wei')
+
+plt.show()
